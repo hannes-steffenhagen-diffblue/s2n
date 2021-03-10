2f2ac86d15b8a202d6079822ce02c825ce57dbc5
Hannes Steffenhagen <leexarmatio@gmail.com>
2021-03-10 14:52:54 +0000

    Add note about makefile insanity in s2n

--- tests/cbmc/proofs/test_runner.py
+++ tests/cbmc/proofs/test_runner.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
+
+# !IMPORTANT!
+# AWS _deliberately_ ignore verification failures
+# for this to work as intended, you need to edit Makefile.common so we do
+# actually see verification failures:
+#
+# DO_AND_LOG_IGNORING_ERROR_10 =	$(1) $(2) 2>&1 | tee $(3); exit $${PIPESTATUS[0]};
+#
+
 import subprocess
 import os
 import sys
+
 
 def run_make(makefile_dir, target):
     with open(os.path.join(makefile_dir, f'{target}.txt'), 'w') as out_file:
